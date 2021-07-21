c85048b851be4e0e68ff93fef9a58790804cb199
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-21 11:11:07 +0100

    Update Main.java

--- src/com/yusuf/calculator/Main.java
+++ src/com/yusuf/calculator/Main.java
@@ -1,8 +1,36 @@
 package com.yusuf.calculator;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int num1 =0;
+        int num2 =0;
+        char operator;
+        double answer = 0.0;
+
+        Scanner scanMath = new Scanner(System.in);
+
+        System.out.println("Please enter your first value");
+        num1 = scanMath.nextInt();
+        System.out.println("Please enter your second value");
+        num2 = scanMath.nextInt();
+        System.out.println("What is your mathematical operation");
+
+        operator = scanMath.next().charAt(0);
+
+        switch (operator) {
+            case '+': answer = num1 + num2;
+                break;
+            case '-': answer = num1 - num2;
+                break;
+            case '*': answer = num1 * num2;
+                break;
+            case '/': answer = num1/num2;
+                break;
+        }
+        System.out.println(num1+" "+operator+" "+num2+" = "+answer);
     }
 }
